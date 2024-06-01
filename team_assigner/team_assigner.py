c2de854dd513cb3d579c17b3dccc9686367a09d8
thomas-terpstra <ta.terpstra@student.han.nl>
2024-06-01 14:16:24 +0200

    posession

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -46,7 +46,7 @@ class TeamAssigner:
             player_color = self.get_player_color(frame, bbox)
             player_colors.append(player_color)
         
-        kmeans = KMeans(n_clusters=2, init="k-means++", n_init=1)
+        kmeans = KMeans(n_clusters=2, init="k-means++", n_init=10)
         kmeans.fit(player_colors)
 
         self.kmeans = kmeans
@@ -63,6 +63,9 @@ class TeamAssigner:
 
         team_id= self.kmeans.predict(player_color.reshape(1, -1))[0]
         team_id+=1
+
+        if player_id == 100:
+            team_id = 1
 
         self.player_team_dict[player_id] = team_id
 
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -3,6 +3,7 @@ import supervision as sv
 import pickle
 import os
 import numpy as np
+import pandas as pd
 import cv2
 import sys
 sys.path.append('../')
@@ -13,6 +14,18 @@ class Tracker:
     def __init__(self, model_path):
         self.model=YOLO(model_path)
         self.tracker = sv.ByteTrack()
+    
+    def interpolate_ball_positions(self, ball_positions):
+        ball_positions = [x.get(1,{}).get('bbox',[]) for x in ball_positions]
+        df_ball_positions = pd.DataFrame(ball_positions, columns=['x1','y1','x2','y2'])
+
+        # Interpolate missing values
+        df_ball_positions = df_ball_positions.interpolate()
+        df_ball_positions = df_ball_positions.bfill()
+
+        ball_positions = [{1:{"bbox":x}} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
     
     def detect_frames(self, frames):
         batch_size =20
@@ -141,7 +154,27 @@ class Tracker:
 
         return frame
 
-    def draw_annotations(self, video_frames, tracks):
+
+    def draw_team_control(self, frame, frame_num, team_ball_control):
+        # Draw a semi transparent rectangle
+        overlay = frame.copy()
+        cv2.rectangle(overlay, (1350 , 850), (1900, 970), (255,255,255), -1)
+        alpha = 0.4
+        cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+
+        team_ball_control_till_frame = team_ball_control[:frame_num+1]
+        # Get the number of times each team has ball control
+        team_1_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==1].shape[0]
+        team_2_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==2].shape[0]
+        team_1 = team_1_num_frames/(team_1_num_frames+team_2_num_frames)
+        team_2 = team_2_num_frames/(team_1_num_frames+team_2_num_frames)
+
+        cv2.putText(frame, f"Team 1 ball posession: {team_1*100:.2f}%",(1400,900), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+        cv2.putText(frame, f"Team 2 ball posession: {team_2*100:.2f}%",(1400,950), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+
+        return frame
+    
+    def draw_annotations(self, video_frames, tracks, team_ball_control):
         output_video_frames = []
         for frame_num, frame in enumerate(video_frames):
             frame = frame.copy()
@@ -152,7 +185,11 @@ class Tracker:
 
             # Draw Players
             for track_id, player in player_dict.items():
-                frame = self.draw_ellipse(frame, player['bbox'], (0,0,255), track_id)
+                color = player.get('team_color', (0,255,0))
+                frame = self.draw_ellipse(frame, player['bbox'],color, track_id)
+
+                if player.get('has_ball', False):
+                    frame = self.draw_triangle(frame, player['bbox'], (0,0,255))
 
             # Draw Referees
             for _, referee in referee_dict.items():
@@ -162,6 +199,9 @@ class Tracker:
             for track_id, ball in ball_dict.items():
                 frame = self.draw_triangle(frame, ball['bbox'], (0,255,0))
 
+            # Draw Team Ball Control
+            frame = self.draw_team_control(frame, frame_num, team_ball_control)
+            
             output_video_frames.append(frame)
         
         return output_video_frames
